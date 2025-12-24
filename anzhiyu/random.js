00e30a765b37559165edba2f2c57295f67197ca5
fuqin <2956509194@qq.com>
2025-12-24 20:08:57 +0800

    Site updated: 2025-12-24 20:08:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["undefined/这是另一个博文/","undefined/这是3/","undefined/这是第二个/","undefined/这是最近文章/","undefined/hello-world/"];function toRandomPost(){
+var posts=["wrnlm/这是另一个博文/","wrnlm/这是3/","wrnlm/这是第二个/","wrnlm/这是最近文章/","wrnlm/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
