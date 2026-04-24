f720a50befeaa68b897e24c22285ddf4ef22754b
12-qwaszx-12 <2956509194@qq.com>
2026-04-24 16:05:51 +0800

    Site updated: 2026-04-24 16:05:49

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1777017529379";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1777017949036";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
